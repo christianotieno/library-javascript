b542609060c05e7f47a4e3f8f5b0aa812f59d6c2
Ermin Cahtarevic <erminc@windowslive.com>
2020-03-10 18:53:56 +0100

    Add Book constructor and add book function

---
+++ js/library.js
@@ -0,0 +1,31 @@
+const id = 0;
+
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id + 1;
+}
+
+const bookList = [];
+
+const submit = document.getElementById('submit');
+
+submit.onclick = () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').checked;
+
+  bookList.push(new Book(title, author, pages, read));
+
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
+  document.getElementById('read').checked = false;
+};
+
+bookList.push(new Book('The great gatsby', 'F. Scott', 255, true));
+bookList.push(new Book('Anna Karenina', 'Leo Tolstoy', 315, false));
+bookList.push(new Book('To Kill a Mockingbird', 'Harper Lee', 192, true));
