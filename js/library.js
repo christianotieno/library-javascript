fa649cd200eb6b31d9af0fc3b612461a3397ab83
ChristianOtieno <cgao273@gmail.com>
2020-03-12 00:07:30 +0300

    implement add books with form button

--- js/library.js
+++ js/library.js
@@ -10,12 +10,24 @@ function Book(author, title, pages, status = false) {
   this.status = status;
 }
 
+myLibrary.push(new Book('author1', 'title1', 123, true));
+myLibrary.push(new Book('author2', 'title2', 123, false));
+myLibrary.push(new Book('author3', 'title3', 123, true));
+
+// displays books added
+function render() {
+  const layout = myLibrary.map(book => bookHtml(book));
+  document.getElementById('book-list').innerHTML = layout;
+}
+
+const addBookButton = document.getElementById('submit');
+
 // updating array
-function addBookToLibrary() {
+submit.onclick = () => {
   const author = document.getElementById('author').value;
   const title = document.getElementById('title').value;
   const pages = document.getElementById('pages').value;
-  const status = document.getElementById('status').value;
+  const status = document.getElementById('status').checked;
 
   myLibrary.push(new Book(author, title, pages, status));
 
@@ -23,16 +35,8 @@ function addBookToLibrary() {
   document.getElementById('title').value = '';
   document.getElementById('pages').value = '';
   document.getElementById('status').value = '';
-}
+  render();
+};
 
-
-myLibrary.push(new Book('author1', 'title1', 123, true));
-myLibrary.push(new Book('author2', 'title2', 123, false));
-myLibrary.push(new Book('author3', 'title3', 123, true));
-
-function render() {
-  const layout = myLibrary.map(book => bookHtml(book));
-  document.getElementById('book-list').innerHTML = layout;
-}
 
 render();
