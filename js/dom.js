e978560027b1cd6b8feae8b4014cc2904e2b9d3c
Ermin Cahtarevic <erminc@windowslive.com>
2020-03-11 20:22:34 +0100

    Add html file structure with table and form for new books

--- js/dom.js
+++
@@ -1,9 +0,0 @@
-export const bookToHTML = (book) => {
-  return `<tr>
-    <td>${book.author}</td>
-    <td>${book.title}</td>
-    <td>${book.pages} pages</td>
-    <td>${book.read ? 'Read' : 'Not read'}</td>
-    <td><button class="btn btn-danger" action="#">submit</button></td>
-  </tr>`
-}
